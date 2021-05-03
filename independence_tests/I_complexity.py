4d31e0eece0af780404adefe871f9a4341a9fb32
Gabriel Goren <gabrielgoren@gmail.com>
2021-05-03 01:04:01 -0300

    fix call to I_complexity.out

--- independence_tests/I_complexity.py
+++ independence_tests/I_complexity.py
@@ -5,7 +5,8 @@ from utils import save_to_temp
 def calculate_I_complexity(binary_string, tempfile='temp/temp'):
     save_to_temp(binary_string, 'text', tempfile=tempfile)
     try:
-        completed_process = subprocess.run(['./I_complexity.out', tempfile, '2'],
+        completed_process = subprocess.run(['./independence_tests/I_complexity.out',
+                                            tempfile, '2'],
                                             capture_output=True,
                                             check=True)
     except subprocess.CalledProcessError as e:
