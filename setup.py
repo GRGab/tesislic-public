5903a7c53f22deb134514f535c642f595039e4a7
Gabriel Goren <gabrielgoren@gmail.com>
2022-10-07 17:51:44 -0300

    fix package name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("DESCRIPTION.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='syntactic-causal-discovery',
+    name='syntactic_causal_discovery',
     version='1.0.0',
     author='Gabriel Goren',
     description='A package for causal discovery using compression as an estimate for algorithmic mutual information',
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url='https://github.com/gabrielgoren/tesislic-public',
     license='GNUv3',
-    packages=['syntactic-causal-discovery'],
+    packages=['syntactic_causal_discovery'],
     install_requires=['numpy',
                       'matplotlib',
                       'scipy',
